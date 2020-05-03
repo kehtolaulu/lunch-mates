e18a1319675e8c8653f16a43e0de4bee7400588a
Emilia Garipova <emilia270627@gmail.com>
2020-05-03 23:16:36 +0300

    Card layout improvements

--- public/lunches.js
+++ public/lunches.js
@@ -3,3 +3,12 @@ const changeColor = (element) => {
     active.classList.remove("active");
     element.classList.add("active");
 };
+
+const openMenu = () => {
+    let item = document.querySelector(".menu-items");
+    if (item.classList.contains("mobile-hidden")) {
+        item.classList.remove("mobile-hidden");
+    } else {
+        item.classList.add("mobile-hidden");
+    }
+};
